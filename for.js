bd99d0ba88cb7b66f31a0f11a6414e58ebe9da31
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-14 15:41:36 -0300

    Add files via upload

---
+++ for.js
@@ -0,0 +1,9 @@
+/*Criar uma variável numero que tem um tipo de dado Number, esse Number é 12
+  Criar um for para se repetir, a quantidade de repetição precisa ser o numero
+  Que está setado na variável numero
+*/ 
+
+var numero = 12;
+for(var nu = 0; nu < numero; ++nu){
+    console.log(`Este código esta sendo executado pela ${nu} vez.`);
+}
